02255b6d81631cba6db7d7e999557eb9de8e8b39
oizquierdo873 <oizquierdo873@gmail.com>
2021-05-09 19:08:47 +0200

    [ADD] Recibo id

--- src/views/Presupuesto.vue
+++ src/views/Presupuesto.vue
@@ -2,41 +2,68 @@
     <div class="container" >
      <h2><b>Realice su presupuesto</b></h2>
         <div class="row">   
-            <div class="col-lg-4" >   
+            <div class="col-lg-4 col-md-4 col-sm-4" >   
                 <div class=" form-group">
                     <div>
                         <label> 1)Cantidad del terreno para trabajar:</label><br>
-                        <input type="radio"> menos de 50 m2 <br>
-                        <input type="radio"> entre 50 y 150 m2<br>
-                        <input type="radio"> entre 150 y 300 m2<br>
-                        <input type="radio"> más de 300 m2<br>
+                        <input type="radio" v-model="a"> menos de 50 m2 <br>   <!-- realizar 5% rebaja -->
+                        <input type="radio" v-model="b"> entre 50 y 150 m2<br> <!-- realizar 10% rebaja -->
+                        <input type="radio" v-model="c"> entre 150 y 300 m2<br><!-- realizar 12% rebaja -->
+                        <input type="radio" v-model="d"> más de 300 m2<br>     <!-- realizar 17% rebaja -->
                     </div>
-                    <button type="button" class="btn btn-danger">Calcular presupuesto</button>
+                    <button type="button" class="btn btn-danger" @click="envioPresupuesto($route.params.id)">Calcular presupuesto</button>
                 </div>
             </div>
-            <div class="col-lg-3 form-group" >
+            <div class="col-lg-3 col-md-3 col-sm-3 form-group" >
                 <div class=" form-group" >
                     <label>Comentarios sobre su presupuesto:</label>
-                    <textarea cols="30" rows="5" placeholder="Escriba..."></textarea>
+                    <textarea cols="30" rows="5" placeholder="Escriba..." v-model="comentario"></textarea>
                 </div>
             </div>
-            <div class="col-lg-3">
+            <div class="col-lg-3 col-md-3 col-sm-3 ">
                 <p>Se calcula el presupuesto</p>
                 <p>El presupuesto es de ... 80 euros</p>
             </div>
         </div>
 
-        <p>ID de servicio recibido: </p>
+        <p>ID de servicio recibido: {{$route.params.id}}</p>
    </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     data(){
         return{
-
+            a: '5%',
+            b: '15%',
+            c: '',
+            d: '',
+            comentario: ''
+         
         }
     },
+
+    methods:{
+        envioPresupuesto(id){
+            let fecha = new Date()
+            console.log("Envio presupuesto: " + fecha);
+            
+            let objeto =  {
+                id_presupuestos: id,
+                cantidad_terreno: this.a,
+                fecha_presupuesto: fecha,
+                comentario: this.comentario,
+                username: 'oscar_bichito',
+                disabled: 1
+            }
+            console.log("Objeto: " , objeto); 
+            axios.post('http://localhost:8080/jardinrobledo/v1/presupuestos/' , objeto)
+            .catch(response => console.log("Error petición insertar: " + response.status));
+
+
+        }
+    }
 
 }
 </script>
