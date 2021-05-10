de7f235e95953df1ba9a122fa132c24ba441450c
oizquierdo873 <oizquierdo873@gmail.com>
2021-05-10 15:41:39 +0200

    [FIX] menu en creación

--- src/views/Administrador.vue
+++ src/views/Administrador.vue
@@ -43,117 +43,14 @@
                     </ul>
                 </div>
             </nav>
-            <div class="col-lg-12  col-md-12  col-sm-12 formulario">
-                <h1 class="text-center">Administrador: </h1>
-            </div>
-        
-            <div class="col-lg-12 col-md-12 col-sm-12 pegado" >
-                <h3 class="text-center"><b>Inserción de Usuario:</b></h3>
-            </div>
-            <div class="col-lg-10 col-md-10 col-sm-10 formulario">
-                <label>Username:</label>
-                <input type="text" class="form-control col-lg-7" placeholder="Usuario..." v-model="user.username">
-                <label>Contraseña:</label>
-                <input type="text" class="form-control col-lg-7" placeholder="contraseña..." v-model="user.password">
-                <button type="button" class="btn btn-success" @click="insertarUsuario()">Agregar nuevo usuario</button>
-            </div>
-            <div class="col-lg-2 col-md-2 col-sm-2 formulario margen">
-                <img src="../imagenes/usuario1.jpg"  width="150" height="150">
-                 <p>{{username}}</p>
-            </div>
-            <div class="alert alert-success" role="alert" v-if="inserccionUsuario === true">
-                <h4 class="alert-heading" >Insertado correctamente</h4>
-            </div>
-
-
-            <!-- SERVICIOS --------------------------------------------------------------------------------->
-            <div class="col-lg-12 col-md-12 col-sm-12 formulario">
-                <h3 class="text-center"><b>Servicios</b></h3>
-                <table class="container-fluid">
-                    <tr class="estiloTable1">
-                        <th>ID</th>
-                        <th>Descripción del servicio</th>
-                        <th>Precio m/€</th>
-                        <th>Modificar</th>
-                            
-                    </tr>
-
-                    <tr v-for="(cadaServicio, index) in arrayServicios" :key="index" class="estiloTable2">
-                        <td>{{cadaServicio.id_servicios}}</td>
-                        <td>{{cadaServicio.descripcion_servicio}}</td>
-                        <td>{{cadaServicio.precio_metro}}</td>
-                        <td>
-                            <button type="button" class="btn btn-success" @click="modificarServicio(cadaServicio.id_servicios)">modificar servicio</button>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-
-
-            <div class="col-lg-12 col-md-12 col-sm-12 pegado" >
-                <h3 class="text-center"><b>Inserción de Servicio</b></h3>
-            </div>
-            <div class="col-lg-10 col-md-10 col-sm-10 formulario">
-                <label>Descripción servicio:</label>
-                <input type="text" class="form-control col-lg-7" placeholder="descripción del nuevo servicio..." v-model="servicio.descripcion_servicio">
-                <label>Precio:</label>
-                <input type="text" class="form-control col-lg-7" placeholder="precio metro" v-model="servicio.precio_metro">
-                <button type="button" class="btn btn-success" @click="nuevoServicio()">Agregar nuevo servicio</button>
-            </div>
-            <div class="col-lg-2 col-md-2 col-sm-2 formulario margen">
-               
-                <img src="../imagenes/desbro.jpg"  width="150" height="150">
-                 <p>{{username}}</p>
-            </div>
-
-            <div class="alert alert-success" role="alert" v-if="aviso === true">
-                <h4 class="alert-heading" >Insertado correctamente</h4>
-            </div>
-            <div class="alert alert-success" role="alert" v-if="modificado === true">
-                <h4 class="alert-heading" >Modificado correctamente</h4>
-            </div>
-            <div class="alert alert-danger" role="alert" v-if="borrado === true">
-                <h4 class="alert-heading" >Borrado correctamente</h4>
-            </div>
-
-            <!-- Presupuestos  ----------------------------------------------------------------------->
-            <div class="col-lg-12 col-md-12 col-sm-12 margenP">
-                <h3 class="text-center"><b>Presupuestos</b></h3>
-                <table class="container-fluid">
-                    <tr class="estiloTable1">
-                        <th>ID</th>
-                        <th>Cantidad terreno</th>
-                        <th>Fecha</th>
-                        <th>Perteneciente a</th>
-                        <th>Disabled </th>
-                        <th>Comentario</th>
-                        <th>Eliminar</th>
-                            
-                    </tr>
-
-                    <tr v-for="(cadaPresupuesto, index) in arrayPresupuestos" :key="index" class="estiloTable2">
-                        <td>{{cadaPresupuesto.id_presupuestos}}</td>
-                        <td>{{cadaPresupuesto.cantidad_terreno}}</td>
-                        <td>{{cadaPresupuesto.fecha_presupuesto}}</td>
-                        <td>{{cadaPresupuesto.username}}</td>
-                        <td>{{cadaPresupuesto.disabled}}</td>
-                        <td>{{cadaPresupuesto.comentario}}</td>
-                        <td>
-
-                            <button type="button" class="btn btn-danger" @click="eliminarPresupuesto(cadaPresupuesto.id_presupuestos,cadaPresupuesto.cantidad_terreno,cadaPresupuesto.fecha_presupuesto,cadaPresupuesto.comentario,cadaPresupuesto.username)">
-                                eliminar
-                            </button>
-                        </td>
-                    
-                    </tr>
-                </table>
-               
-            </div>
-            <div class="alert alert-danger" role="alert" v-if="borradoPresupuesto === true">
-                <h4 class="alert-heading" >Borrado correctamente</h4>
-            </div>
-
         </div>
+          <div id="nav">
+            <router-link to="/usuarios">usuarios</router-link> |
+            <router-link to="/insertarUsuario">Inertar usuarios</router-link> |
+            <router-link to="/modificarUsuario">Modificar usuarios</router-link> |
+            <router-link to="/eliminacionUsuario">eliminar usuario</router-link>|
+        </div>
+            <router-view/>
     </div>
 
 </template>
@@ -316,7 +213,6 @@ export default {
     background-color: black;
     color: white;
 }
-/**background-color: antiquewhite; */
 .estiloTable2{
     border: solid black 1px;
 }
