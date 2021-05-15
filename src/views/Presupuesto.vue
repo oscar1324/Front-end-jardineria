05695f63ac2811c3086caf8cc6d01ddd0d54f282
oizquierdo873 <oizquierdo873@gmail.com>
2021-05-15 09:50:20 +0200

    [FIX] Cambio de estilos

--- src/views/Presupuesto.vue
+++ src/views/Presupuesto.vue
@@ -1,37 +1,39 @@
 <template>
     <div class="container" >
-     <h2><b>Realice su presupuesto</b></h2>
+     <h1 class="text-center"><b>Realice su presupuesto</b></h1>
         <div class="row">   
-            <div class="col-lg-3 col-md-4 col-sm-4" >   
+            <div class="col-lg-5 col-md-4 col-sm-4" >   
                 <div class=" form-group">
                     <h4>¿Cual es tu terreno?</h4>
                     <label for="enviarFormulario">
                         <input type="radio" id="1" value="50" v-model="tipo">
                         <span>Menos de 50 m2 </span>
-                    </label> <br>
+                    </label> <br><br>
                     <label for="enviarFormulario">
                         <input  type="radio" id="2" value="100"  v-model="tipo">
                         <span>Entre 50 y 150 m2</span>
-                    </label> <br>
+                    </label> <br><br>
                     <label for="enviarFormulario">
                         <input  type="radio" id="2" value="200"  v-model="tipo">
                         <span>Entre 150 y 300 m2</span>
-                    </label> <br>
-                    <label for="enviarFormulario">
+                    </label> <br><br>
+                    <label for="enviarFormulario" class="labelito">
                         <input  type="radio" id="2" value="500"  v-model="tipo">
                         <span>Más de 300 m2</span>
                     </label> <br>
                 </div>
-                <button type="button" class="btn btn-danger" @click="envioPresupuesto($route.params.id,$route.params.usuario )">Calcular presupuesto</button>
-
+               
             </div>
  
-            <div class="col-lg-5 col-md-4 col-sm-4 form-group text-center" >
+            <div class="col-lg-7 col-md-4 col-sm-4 form-group labelito " >
                 <h4>Comentarios sobre su terreno:</h4>
-                <textarea cols="30" rows="5" placeholder="Escriba..." v-model="comentario"></textarea>
+                <textarea cols="30" rows="5" placeholder="Escriba..." class="form-control" v-model="comentario"></textarea>
             </div>
+            <button type="button" class="btn btn-danger" @click="envioPresupuesto($route.params.id,$route.params.usuario )">Calcular presupuesto</button>
+
      
-            <div class="col-lg-3 col-md-3 col-sm-3 text-center">
+            <div class="col-lg-12 col-md-3 col-sm-3 text-center">
+                 <br>
                 <h4>Presupuesto de:</h4>
                 <p>El presupuesto es de ... 80 euros</p>
             </div>
@@ -78,11 +80,14 @@ export default {
 </script>
 <style scoped>
 .container{
-    color: black;
+    color: white;
     background-image: url(https://i.pinimg.com/originals/32/59/b4/3259b419f4e8f069b2fd6aefb9f15ae0.jpg);
 
     /*https://i0.wp.com/data2.1freewallpapers.com/download/singapore-botanic-gardens-walking-paths.jpg, esta foto de gana de moomento*/
     /* https://i.pinimg.com/originals/32/59/b4/3259b419f4e8f069b2fd6aefb9f15ae0.jpg   - foto flor*/
-    
+    height: 800px;
+}
+.labelito{
+    margin-bottom: 30px;
 }
 </style>
