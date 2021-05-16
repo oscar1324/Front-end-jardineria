ecf113ec5ba4a373617b7fcec316ea32a1e5a0a4
oizquierdo873 <oizquierdo873@gmail.com>
2021-05-16 21:03:29 +0200

    [FIX] Revisión insercción erronea

---
+++ src/components/tablaModificarServicios.vue
@@ -0,0 +1,59 @@
+<template>
+                <div class="col-lg-12">
+                 <div class="col-lg-12 col-md-12 col-sm-12 pegado" >
+                <h3 class="text-center"><b>Modificación de precio:</b></h3>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 formulario">
+                <label>Descripción servicio:</label>
+                <input type="text" class="form-control col-lg-7" placeholder="descripción del nuevo servicio..." v-model="servicio.descripcion_servicio">
+                <label>Precio:</label>
+                <input type="text" class="form-control col-lg-7" placeholder="precio metro" v-model="servicio.precio_metro">
+                <button type="button" class="btn btn-success" @click="modificarServicio()">Modificar precio</button>
+            </div>
+            <p>aqui: {{idRecibido}} - {{nombreRecibido}}</p>
+            <div class="alert alert-success" role="alert" v-if="aviso === true">
+                <h4 class="alert-heading" >Modificado correctamente</h4>
+            </div>
+            </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            
+            aviso:false,
+            servicio: {
+                id: this.idRecibido,
+                descripcion_servicio : this.nombreRecibido, 
+                precio_metro: '',
+            },
+        }
+    },
+    props:['idRecibido', 'nombreRecibido'],
+    methods:{
+        modificarServicio(){
+            axios.put('http://localhost:8080/jardinrobledo/v1/servicios/', this.servicio)
+            .then( response =>{
+                console.log(response);
+            })
+            .catch(response => console.log("Error petición modificar: " + response.status));
+            console.log("Objeto: " , this.servicio);
+            this.aviso = true;
+            setTimeout(()=>{
+                this.aviso = false;
+            }, 3500);
+         
+        },
+    },
+        updated(){
+        axios.get('http://localhost:8080/jardinrobledo/v1/servicios')
+        .then( response => {
+            this.arrayServicios = response.data;
+            this.arrayServicios.sort(((a,b) => b.precio_metro - a.precio_metro));
+        })
+        .catch(response => alert("Error petición obtener: " + response.status));
+    },
+}
+</script>
