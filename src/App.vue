7a72098f0f6b8d41d4b4b7ec4a83fb80135fcead
oizquierdo873 <oizquierdo873@gmail.com>
2021-05-17 18:35:35 +0200

    [ADD] Estilos menú

--- src/App.vue
+++ src/App.vue
@@ -14,26 +14,38 @@
           <a href="/Principal" class="navbar-brand ">
             <img src="./imagenes/flor.jpg" width="170" height="70" class="col-lg-12 col-md-12 col-sm-12 col-xs-12 imagen">
           </a><br>
-          <router-link to="/Principal" class="enlace">Inicio</router-link>            <br> 
-          <router-link to="/servicio/:usuario" class="enlace">Servicios</router-link> <br> 
-          <router-link to="/Presupuesto" class="enlace">Presupuesto</router-link>     <br> 
-          <router-link to="/Contacto" class="enlace">Contacto</router-link>           <br> 
-          <router-link to="/login" class="enlace">Login</router-link>                 <br> 
-          <hr>
-          <h5 v-if="$route.params.usuario === '@Administrador'" class="apartado">Usuarios:</h5><br>
-          <router-link v-if="$route.params.usuario === '@Administrador'" to="/usuarios/@Administrador" class="enlace">Listar </router-link> <br> 
-          <router-link v-if="$route.params.usuario === '@Administrador'" to="/modificarUsuario/@Administrador" class="enlace">Modificar </router-link> <br> 
-          <router-link v-if="$route.params.usuario === '@Administrador'" to="/eliminacionUsuario/@Administrador" class="enlace">Eliminar </router-link> <br> 
-          <hr>
-          <h5 class="apartado" v-if="$route.params.usuario === '@Administrador'">Servicios:</h5><br>
-          <router-link v-if="$route.params.usuario === '@Administrador'" to="/serviciosList/@Administrador" class="enlace" >Listar </router-link> <br> 
-          <router-link v-if="$route.params.usuario === '@Administrador'" to="/modificarServicio/@Administrador" class="enlace">Modificar </router-link> <br> 
-          <hr>
-          <h5 class="apartado" v-if="$route.params.usuario === '@Administrador'">Presupuestos:</h5><br>
-          <router-link v-if="$route.params.usuario === '@Administrador'" to="/listPresupuestos/@Administrador" class="enlace">Listar </router-link> <br> 
-          <router-link v-if="$route.params.usuario === '@Administrador'" to="/eliminarPresupuesto/@Administrador" class="enlace">Eliminar </router-link>
+          <div class=" nav">
+            <router-link to="/Principal" class="paraUsu">Inicio</router-link> <br>     
+            <p>.</p>
+            <router-link to="/servicio/:usuario" class="paraUsu">Servicios</router-link> <br> 
+            <router-link to="/Contacto" class="paraUsu">Contacto</router-link>           <br> 
+            <router-link to="/login" class="paraUsu">Login</router-link>  
+          </div>  
+          <hr v-if="$route.params.usuario === '@Administrador'">
+          <div class="apartado col-lg-12 col-md-12 col-sm-12" v-if="$route.params.usuario === '@Administrador'">
+            <h5 class="enunciado" v-if="$route.params.usuario === '@Administrador'" >Usuarios</h5>
+            <router-link v-if="$route.params.usuario === '@Administrador'" to="/usuarios/@Administrador" class="enlace">Listar </router-link> <br> 
+            <router-link v-if="$route.params.usuario === '@Administrador'" to="/modificarUsuario/@Administrador" class="enlace">Modificar </router-link> <br> 
+            <router-link v-if="$route.params.usuario === '@Administrador'" to="/eliminacionUsuario/@Administrador" class="enlace">Eliminar </router-link> <br> 
+          </div>
+         
+          <hr v-if="$route.params.usuario === '@Administrador'">
+
+          
+          <div class=" apartado col-lg-12 col-md-12 col-sm-12" v-if="$route.params.usuario === '@Administrador'">
+            <h5 class="enunciado" v-if="$route.params.usuario === '@Administrador'" >Servicios</h5>
+            <router-link v-if="$route.params.usuario === '@Administrador'" to="/serviciosList/@Administrador" class="enlace" >Listar </router-link> <br> 
+            <router-link v-if="$route.params.usuario === '@Administrador'" to="/modificarServicio/@Administrador" class="enlace">Modificar </router-link> <br> 
+          </div>
+          
+          <hr v-if="$route.params.usuario === '@Administrador'">
+          <div class="apartado" v-if="$route.params.usuario === '@Administrador'">
+            <h5 class="enunciado" v-if="$route.params.usuario === '@Administrador'">Presupuestos</h5>
+            <router-link v-if="$route.params.usuario === '@Administrador'" to="/listPresupuestos/@Administrador" class="enlace">Listar </router-link> <br> 
+            <router-link v-if="$route.params.usuario === '@Administrador'" to="/eliminarPresupuesto/@Administrador" class="enlace">Eliminar </router-link>
+          </div>
       </div>
-      <div class=" col-lg-10 col-md-10 col-sm-10 col-xs-10 centro">
+      <div class="centro col-lg-10 col-md-10 col-sm-10 col-xs-10 ">
         <router-view></router-view>
       </div>
       <Footer/>
@@ -45,6 +57,11 @@
 <script>
 import Footer from '../src/components/Footer.vue';
 export default {
+  data(){
+    return{
+    
+    }
+  },
   components: {Footer},
   emits: 'nombreUsuario'
   
@@ -60,30 +77,58 @@ export default {
 
 }
 .enlace{
-  font-size: 150%;
+  font-size: 5mm;
   color: white;
+  text-decoration:none;
+}
+.enunciado{
+   font-family: fantasy;
 }
 .cabeza{
-    background-color: rgb(11, 15, 15);
+  margin-top: 10px;
+    background-color: #313131;
     color: white;
+     border-radius: 20px;
 }
 .menu{
   margin-top: 10px;
-  background-color: #080808;
+  background-color: #313131;
   margin-bottom: 10px;
+   border-radius: 20px;
 }
 .centro{
    margin-top: 10px;
    margin-bottom: 10px;
+  border-radius: 20px;
   
 }
 .apartado{
   font-size: 170%;
   color: antiquewhite;
+    padding: 3px 10px;
+  border: PowderBlue 5px solid;
+  border-top-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  
 }
 .imagen{
   margin-right: 5px;
    margin-top: 10px;
+}
+.paraUsu {
+  cursor: pointer;
+  color: white;
+  font-size: 7mm;
+  text-decoration:none;
+}
+.nav a{
+    font-size: 7mm;
+
+  color: white;
+}
+.nav a.router-link-exact-active {
+    font-size: 7mm;
+  color: #42b983;
 }
 </style>
 
